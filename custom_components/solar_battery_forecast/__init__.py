253021a9ee4e9cc561b4537f3e2169b5c4f3aa41
Antony Male <antony.male@gmail.com>
2025-07-09 15:48:09 +0100

    Fix async_forward_entry_setup deprecation

--- custom_components/solar_battery_forecast/__init__.py
+++ custom_components/solar_battery_forecast/__init__.py
@@ -50,9 +50,7 @@ async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
     else:
         hass.async_create_task(controller.load(dt.now()))
 
-    for platform in PLATFORMS:
-        if entry.options.get(platform, True):
-            hass.async_add_job(hass.config_entries.async_forward_entry_setup(entry, platform))
+    hass.async_add_job(hass.config_entries.async_forward_entry_setups(entry, PLATFORMS))
 
     entry.async_on_unload(entry.add_update_listener(async_reload_entry))
     return True
